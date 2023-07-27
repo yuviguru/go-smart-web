3426aeb5e09a44dc2f9afe8c89a53d778d168b07
Yuvaraj <yuvaguru.guru8@gmail.com>
2023-07-28 02:37:36 +0530

    Fix class attribute

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -7,20 +7,20 @@ const About = () => {
         <section id="about">
             <div className='container is-fullhd body-container_about'>
                 <StaticImage className='about-us-image' src="../svg/rocket.svg" alt="AboutGoSmart" />
-                <h4 class="title is-4 has-text-centered section-title">ABOUT GO SMART</h4>
-                <div class="tile is-ancestor">
-                    <div class="tile is-horizontal">
-                        <div class="tile is-parent">
-                            <div class="tile is-child box"> vestibulum at eros. Donec id elit non mi porta gravida at eget metus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras mattis consectetur purus sit amet fermentum.
+                <h4 className="title is-4 has-text-centered section-title">ABOUT GO SMART</h4>
+                <div className="tile is-ancestor">
+                    <div className="tile is-horizontal">
+                        <div className="tile is-parent">
+                            <div className="tile is-child box"> vestibulum at eros. Donec id elit non mi porta gravida at eget metus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras mattis consectetur purus sit amet fermentum.
                             </div>
                         </div>
-                        <div class="tile is-parent">
-                            <div class="tile is-child box">
+                        <div className="tile is-parent">
+                            <div className="tile is-child box">
                                 Lorem ipsum leo risus, porta ac consectetur ac, vestibulum at eros. Donec id elit non mi porta gravida at eget metus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras mattis consectetur purus sit amet fermentum.
                             </div>
                         </div>
-                        <div class="tile is-parent">
-                            <div class="tile is-child box">
+                        <div className="tile is-parent">
+                            <div className="tile is-child box">
                                 Lorem ipsum leo risus, porta ac consectetur ac, vestibulum at eros. Donec id elit non mi porta gravida at eget metus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras mattis consectetur purus sit amet fermentum.
                             </div>
                         </div>
