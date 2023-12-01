896ab7d3c7b6e76fec369b299c28ccaf86648082
Yuvaraj <yuvaguru.guru8@gmail.com>
2023-12-01 13:23:08 +0530

    Course section

--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -1,18 +1,42 @@
 import React from 'react';
+import "../styles/contact.scss";
+import { StaticImage } from "gatsby-plugin-image";
 
 console.log('contact component');
 
 export default function Contact() {
-  return (<section id="contact">
-  <div className='container is-fullhd'>
-    <div className='columns'>
-      <div className='column is-half'>
-      </div>
-      <div className='column is-half courses-slider'>
-          <h1 className='body-content_title'>Our Courses</h1>
-          <p className='body-content_description'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+  return (
+  <section id="contact">
+    <div className='container is-fullhd body-container_contact'>
+      <div className='columns'>
+        <div className='column is-three-fifths'>
+            <h1 className='body-content_title'>CONTACT US</h1>
+            <p className='body-content_description'>PHONE NUMBER</p>
+            <p className='body-content_description'>MAIL ID</p>
+            <p className='body-content_description'>ADDRESS</p>
+            <h2 className='body-content_title'>USEFUL LINKS</h2>
+            <div className='is-flex use-full_links'>
+              <p className='body-content_description'>HOME</p>
+              <p className='body-content_description'>OUR COURSES</p>
+              <p className='body-content_description'>GALLERY</p>
+              <p className='body-content_description'>NEWS AND EVENTS</p>
+              <p className='body-content_description'>ABOUT</p>
+            </div>
+        </div>
+        <div className='column is-two-fifths'>
+            <StaticImage className='contact-us_image' src="../svg/rainbow.svg" alt="ContactGoSmart" />
+            <h1 className='body-content_title has-text-centered'>GET IN TOUCH </h1>
+            <div className='contact_form has-text-right'>
+              <input className="input" placeholder='NAME' name='name' />
+              <input className="input" placeholder='MAIL ID' name='mailId' />
+              <textarea className='textarea' placeholder='MESSAGE' name='message'></textarea>
+              <button className='button is-normal is-responsive'>
+                Submit
+              </button>
+            </div>
+        </div>
       </div>
     </div>
-  </div>
-</section>);
+  </section>
+);
 }
---
+++ src/locales/constants.js
@@ -0,0 +1,8 @@
+const strings = {
+  HOME_BANNER_TITLE: "Welcome to our website!",
+  HOME_BANNER_DESCRIPTION: "sdsd",
+  HOME_BANNER_ACTION: "Learn more about us",
+  contact: "Contact us",
+};
+  
+export default strings;
