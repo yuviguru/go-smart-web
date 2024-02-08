19038e272bcc212a634e8cf4b582680b2744bb9f
Yuvaraj <yuvaguru.guru8@gmail.com>
2024-02-08 09:34:45 +0530

    Update styles for logo

--- src/components/HomeSection.js
+++ src/components/HomeSection.js
@@ -9,8 +9,8 @@ export default function Home() {
       <div className='container is-fullhd body-container_home'>
           <div className='body-content_home'>
             <h1 className='body-content_title'>BE SMART WITH<br/>
-              <StaticImage src="../images/mini-logo.png" alt="GoSmartLogo" transformOptions="{}"/>
             </h1>
+              <StaticImage src="../images/mini-logo.png" className="body-content_logo" alt="GoSmartLogo" transformOptions='{fit: "cover", cropFocus: "attention"}' width={450} layout='constrained'/>
             <p className='body-content_description'>Unleashing Potential, Inspiring Brilliance. Our tailored education fosters critical thinking and creativity. Choose a future of limitless possibilities with Go Smart's holistic approach. Elevate your child's journey – where intelligence meets innovation."</p>
             <button className="button is-normal is-responsive">
               Get in touch
